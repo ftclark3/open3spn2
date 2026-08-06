3fad5e92946079686a1eb6188911e7ed2a190637
Finley Clark <60708336+ftclark3@users.noreply.github.com>
2026-08-05 19:16:26 -0500

    support CustomCVForce setup for BasePair and CrossStacking in a back-compatible way

--- open3SPN2/force/dna.py
+++ open3SPN2/force/dna.py
@@ -258,12 +258,12 @@ class BasePair(DNAForce, openmm.CustomHbondForce):
                 if (atom_a.chainID == atom_b.chainID) and (abs(atom_a.resSeq - atom_b.resSeq) <= 2):
                     self.forces[i].addExclusion(atom_a['donor_id'], atom_b['aceptor_id'])
 
-    def addForce(self, system, name=None):
-        for f in self.forces:
+    def addForce(self, system, names=None):
+        for counter, f in enumerate(self.forces):
             try:
                 system.addForce(self.forces[f])
             except AttributeError:
-                system.addCollectiveVariable(name, self.forces[f]) # our "system" might actually be a CustomCVForce
+                system.addCollectiveVariable(names[counter], self.forces[f]) # our "system" might actually be a CustomCVForce
 
 
 class CrossStacking(DNAForce):
@@ -414,14 +414,14 @@ class CrossStacking(DNAForce):
                         c1.addExclusion(ii, jj)
                         c2.addExclusion(jj, ii)
 
-    def addForce(self, system, name=None):
-        for c1, c2 in self.crossStackingForces.values():
+    def addForce(self, system, names=None):
+        for counter, (c1, c2) in enumerate(self.crossStackingForces.values()):
             try:
                 system.addForce(c1)
                 system.addForce(c2)
-            except AttributeError:
-                system.addCollectiveVariable(name, c1) # our "system" might actually be a CustomCVForce
-                system.addCollectiveVariable(name, c2) # our "system" might actually be a CustomCVForce
+            except AttributeError: # names should be a list of lists
+                system.addCollectiveVariable(names[counter][0], c1) # our "system" might actually be a CustomCVForce
+                system.addCollectiveVariable(names[counter][1], c2) # our "system" might actually be a CustomCVForce
 
     def getForceGroup(self):
         fg = 0
